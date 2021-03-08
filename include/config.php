a67272f10c708aadbcc63e9bd2bd10e1d77de2e3
winnie258 <78522105+winnie258@users.noreply.github.com>
2021-03-08 19:34:30 +0800

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php
+$host = 'localhost'; //数据库地址
+$port = 3306; //数据库端口
+$user = 'root'; //数据库用户名
+$pwd = 'root'; //数据库密码
+$dbname = 'root'; //数据库名
+if($pwd=$dbname) die('您还未配置include/config.php中的数据库信息');
+?>
